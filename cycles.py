77d8ddc31b6ab6ae35bf920272df3ed605b23aa3
Anvar-dot <samatovanvar722@gmail.com>
2025-07-28 13:56:55 +0600

    bla bla bal

--- cycles.py
+++ cycles.py
@@ -32,12 +32,60 @@
 # print("\nОбратный отсчет завершен!")
 
 
-start_num = int(input("Введите первое число: "))
-end_num = int(input("Введите второе число: "))
-total_num = 0
+# start_num = int(input("Введите первое число: "))
+# end_num = int(input("Введите второе число: "))
+# total_num = 0
 
-while start_num <= end_num:
-    if start_num % 2 == 0:
-        total_num += start_num
-    start_num += 1
+# while start_num <= end_num:
+#     if start_num % 2 == 0:
+#         total_num += start_num
+#     start_num += 1
     
+    
+    
+# while True:
+#     for i in range(1, 10):
+#         if i % 2 == 0:
+#             print(i, end=' ')
+#         else:
+#             if i ==10:
+#                 break
+        
+# print("\nСчет успешно завершен !")
+
+
+
+# i = 1
+# while i <= 10:
+#     if i % 2 == 0:
+#         print(i, end=' ')
+#     i += 1
+
+# print("\nСчет успешно завершен!")
+
+
+# i = 1
+# while i <=25:
+#     if i % 2 == 0:
+#         print(i,end=' ')
+#     i += 3
+    
+# print("\nСчет успешно завершен!")
+
+
+
+
+password = "agent007"
+get_password = input("Введите пароль: ")
+
+
+while get_password != password:
+    if get_password == "exit":
+        print("Вы вышли из программы. ")
+        break
+    else:
+        print("Неверный пароль. Попробуйте еще раз.")
+        get_password = input("Введите пароль: ")
+        
+else:
+    print("Добро пожаловать в систему!")
