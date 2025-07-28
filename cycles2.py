9e4196f60627712ac8f2e4eb724c45823af83631
Anvar-dot <samatovanvar722@gmail.com>
2025-07-28 14:28:56 +0600

    oooh yes

--- cycles2.py
+++ cycles2.py
@@ -1,2 +1,35 @@
 # cycle for
+# строка это последовательность символов 
+# цикл for используется для перебора элементов в последовательности
 
+# text = "Hello, world!"
+# # print(text[0:5])
+
+
+# for перменная in последовательность :
+#     действия
+
+# for i in text:
+#     print(i, end=' ')
+    
+#range это функция которая создает последовательность чисел
+# range(start, stop, step)
+# start - начальное значение (включительно)
+# stop - конечное значение (исключительно)
+# step - шаг (по умолчанию 1)
+
+# for i in range(0,21,3):
+#     print(i)
+
+# num = int(input("Введите число: "))
+
+# for i in range(1, 11):
+#     result = num * i
+#     print(f"{num} * {i} = {result}")
+
+
+for i in range(11,1, -1):
+    for j in range(1, i -1):
+        print(j, end=' ')
+    print()
+    
