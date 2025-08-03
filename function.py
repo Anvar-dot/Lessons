2554c4cb30499c69edf7b282d2f2207c88c4699f
Anvar-dot <samatovanvar722@gmail.com>
2025-08-03 15:54:42 +0600

    new function

--- function.py
+++ function.py
@@ -1,14 +1,27 @@
-password = input("Введите пароль: ")
+# # function.py
+# def check_password(password):
+#     if len(password) < 8:
+#         return "Используйте пароль длиной не менее 8 символов."
+#     elif not any(char.isdigit() for char in password):
+#         return "Пароль должен содержать хотя бы одну цифру."
+#     elif not any(char.isalpha() for char in password):
+#         return "Пароль должен содержать хотя бы одну букву."
+#     else:
+#         return "Пароль принят."
+    
+    
+# while True:
+#     password = input("Введите пароль: ")
+#     result = check_password(password)
+#     print(result)
+#     if result == "Пароль принят.":
+#         break
 
-def check_password(password):
-    if len(password) < 8:
-        return "Используйте пароль длиной не менее 8 символов."
-    elif not any(char.isdigit() for char in password):
-        return "Пароль должен содержать хотя бы одну цифру."
-    elif not any(char.isalpha() for char in password):
-        return "Пароль должен содержать хотя бы одну букву."
+
+def is_even(number):
+    if number % 2 ==0:
+        return "Число четное."
     else:
-        return "Пароль принят."
+        return "Число нечетное."
     
-    
-print(check_password(password))
+print(is_even(123))
