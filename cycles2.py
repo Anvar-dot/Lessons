b16f1d7b638132751686e461bd18a7c38e057636
Anvar-dot <samatovanvar722@gmail.com>
2025-08-04 10:23:38 +0600

    asihbq

--- cycles2.py
+++ cycles2.py
@@ -28,8 +28,19 @@
 #     print(f"{num} * {i} = {result}")
 
 
-for i in range(11,1, -1):
-    for j in range(1, i -1):
-        print(j, end=' ')
-    print()
+# for i in range(11,1, -1):
+#     for j in range(1, i -1):
+#         print(j, end=' ')
+#     print()
     
+    
+import random
+import string
+
+def generate_password(length=10):
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+print(generate_password())
+print(f"Сгенерированный пароль: {generate_password(20)}")
